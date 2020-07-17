d0f4eb494a471bedeeddb69c6b215a33cff36064
Brandon Pollack <brandonpollack23@gmail.com>
2020-07-17 06:31:52 +0000

    Fixes with $

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ warning_text="# === THIS FILE IS GENERATED DO NOT EDIT, EDIT TEMPLATE INSTEAD ==
 echo $warning_text > /etc/nginx/nginx.conf
 
 # This will generate the conf file 
+export DOLLAR='$'
 envsubst < ./nginx.conf.template >> /etc/nginx/nginx.conf
 
 # Run the CMD
